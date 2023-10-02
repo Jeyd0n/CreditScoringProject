1e2b3a0539069d8f634078d10768c1b8ae7782e9
Jeyd0n <polukhin.vo@icloud.com>
2023-10-02 19:35:10 +0300

    Import base
    Import cookiecutter base for ds projects and load raw data for train and test

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Pet-project for solution credit scroing problem. A part of student project in RANEPA university',
+    author='Vladimir Polukhin',
+    license='MIT',
+)
